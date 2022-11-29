94ab515d6c2a8b03d1eee03cca06ed2a182ffd19
GoldLuckyK <talentlucky0816@gmail.com>
2022-11-30 01:37:08 +0900

    [updated]draw_a_league_2022

--- ladder_a_league_2022.php
+++ ladder_a_league_2022.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(__DIR__.'/library/common.lib.php');
+require_once(__DIR__ . '/library/common.lib.php');
 
 $neccessery_data = array(
 	"root" => "https://soccer.sportmonks.com/api/v2.0",
@@ -11,27 +11,72 @@ $neccessery_data = array(
 	"season_id" => 20243,
 );
 
-function get_ladder_data() {
+function get_ladder_data()
+{
 	global $neccessery_data;
 	$curl = curl_init();
 
 	curl_setopt_array($curl, array(
-		CURLOPT_URL => $neccessery_data['root'].'/standings'.'/season/'.$neccessery_data['season_id'].'?'.$neccessery_data['token'],
+		CURLOPT_URL => $neccessery_data['root'] . '/standings' . '/season/' . $neccessery_data['season_id'] . '?' . $neccessery_data['token'],
 		CURLOPT_RETURNTRANSFER => true
 	));
 
-	$result = json_decode(curl_exec($curl))->data;
+	$result = json_decode(curl_exec($curl))->data[0];
 	return $result;
 }
 
-function __process_ladder_data($result) {
-	
+function __process_competition_data($result)
+{
 
-	return $ladder;
+	$competition = new \stdClass;
+
+	$competition->league_id = $result->league_id;
+	$competition->competition_ref_id = 214;
+	$competition->season_code = $result->season_id;
+	$competition->round_code = 0;
+
+	$ladders = [];
+	foreach ($result->standings->data as $standing) {
+		$ladder = new \stdClass;
+
+		$ladder->team_id = 	$standing->team_id;
+		$ladder->next_team_id = 0;
+		$ladder->ladder_position = 	$standing->position;
+		$ladder->stats_played = $standing->overall->games_played;
+		$ladder->stats_wins = $standing->overall->won;
+		$ladder->stats_drawn = $standing->overall->draw;
+		$ladder->stats_lost = $standing->overall->lost;
+		$fld_name = "goals_scored";
+		$ladder->stats_points_for = $standing->overall->$fld_name;
+		$fld_name = "goals_against";
+		$ladder->stats_points_against = $standing->overall->$fld_name;
+		$ladder->stats_bonus_points = 0;
+		$ladder->stats_points = $standing->points;
+
+		$ladders[] = $ladder;
+	}
+
+	$competition->ladder = $ladders;
+
+	return $competition;
 }
 
 while (true) {
 	$result = get_ladder_data();
-	$ladder = __process_ladder_data($result);
+	$ladder = __process_competition_data($result);
 
+	@file_put_contents('ladder_a-league_2022.json', json_encode($ladder, JSON_PRETTY_PRINT));
+
+	var_dump($ladder);
+	exit;
+
+	sendLadderToServer($ladder);
+
+	sleep(3);
+
+	$game_status = @file_get_contents("/var/www/MatesPicks/public/banter/game_status");
+	if (!$game_status) {
+		sleep(60);
+		continue;
+	}
 }
